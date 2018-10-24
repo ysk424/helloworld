29e31774ff65513659915c15c96f5bb5e34545f6
Yoshihiko Tsukamoto <ysk424@hotmail.com>
2018-10-24 19:30:12 +0900

    version2

--- helloworld/Program.cs
+++ helloworld/Program.cs
@@ -6,7 +6,7 @@ namespace helloworld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!vresion 2");
         }
     }
 }
